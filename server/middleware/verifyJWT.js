5210ce6d0d1f68492abe23d888c54caafac0df57
chankyu kim <chank07@naver.com>
2022-04-13 10:58:56 +0900

    jwt 인증 추가

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -2,17 +2,21 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  if (!authHeader) return res.sendStatus(401);
-  console.log(authHeader); // Bearer toekn
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    console.log('decoded', decoded);
-    if (err) return res.sendStatus(403); //invalid token
-    req.user = decoded.username;
-    console.log('req.user', req.user);
-    next();
-  });
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+  if (!authHeader.startsWith('Bearer ')) return res.sendStatus(401);
+  const token = authHeader.split(' ')[1]; //Bearer token
+  jwt.verify(
+    token, //
+    process.env.ACCESS_TOKEN_SECRET,
+    (err, decoded) => {
+      // console.log('decoded', decoded);
+      if (err) return res.sendStatus(403); //invalid token
+      req.user = decoded.UserInfo.username;
+      req.roles = decoded.UserInfo.roles;
+      // console.log('req.user', req.user);
+      next();
+    }
+  );
 };
 
 module.exports = verifyJWT;
